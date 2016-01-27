630174bf03c6c3a5ee822e391f99706ef6f7fc68
Nicolas Liampotis <nliam@grnet.gr>
2016-01-27 12:17:42 +0200

    Add `Cache-Control: no-cache` header to response

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get('/jenkins/c/http/*', function(req,res) {
           }
           console.log('[GET] ' + '/jenkins/c/http/' + jurl)
           console.log('      generating badge(' + badge_url + ')')
+          res.setHeader('Cache-Control', 'no-cache');
           res.redirect(badge_url)
         }
       }
